512ae23a7674dd6977395f501cdc7d13808d8b04
Potpally Vinay <potpallyvinay1@gmail.com>
2024-07-26 09:18:50 +0530

    spring boot commit

--- src/main/java/com/commerce/fashion/FashionApplication.java
+++ src/main/java/com/commerce/fashion/FashionApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FashionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FashionApplication.class, args);
 	}
-
 }
